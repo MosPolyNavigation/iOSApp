077e0e73671fa0197cecccb75919cd809bcf84d5
miv-dev <i@mike39.ru>
2023-04-16 17:19:15 +0300

    Initial Commit

---
+++ Poly Nav/Poly_NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poly_NavApp.swift
+//  Poly Nav
+//
+//  Created by Михаил on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Poly_NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
